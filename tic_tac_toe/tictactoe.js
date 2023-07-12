1f36bd5f3118a972a64ead22acbfe593757ab09c
Immanjeba <immanjeba98@gmail.com>
2023-07-12 17:45:23 +0530

    select letter task finished

--- tic_tac_toe/tictactoe.js
+++ tic_tac_toe/tictactoe.js
@@ -15,10 +15,35 @@ while(chooseToss == null){
 var selectToss = Math.floor(Math.random()*2)
 
 var person = null
-var Computer = null
+var computer = null
 if(chooseToss == selectToss){
     console.log("You Win")
+    console.log("Your Turn")
+    while(person == null){
+        person = prompt("what letter would you like to choose 'x' or 'o': ")
+        if(person == "o" || person == "x"){
+            break
+        }
+        console.log("You've chosen wrong")
+        person = null
+    }
 
 } else {
     console.log("Computer Win")
+    console.log("Computer Turn")
+    computer = Math.floor(Math.random()*2)
+    if(computer == 1){
+        computer = "x"
+    } else {
+        computer = "o"
+    }
+
+}
+
+if(person = "x" || computer == "o"){
+    console.log("Your letter is X")
+    console.log("computer letter is O")
+} else {
+    console.log("Your letter is O")
+    console.log("computer letter is X")
 }
